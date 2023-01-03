200c16a7614d6a9d620ca587394b943e74d7d393
Jakob Petermandl <e11776823@student.tuwien.ac.at>
2023-01-03 14:00:35 +0100

    initial commit

---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+# How to run
+### First run
+```bash
+$ mvn clean install
+```
+### Then
+```bash
+$ mvn spring-boot:run
+```
---
+++ src/test/java/ac/at/tuwien/streamingsocketbackend/StreamingSocketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.at.tuwien.streamingsocketbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingSocketBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
