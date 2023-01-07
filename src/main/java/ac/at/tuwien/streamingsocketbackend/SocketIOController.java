468c198aeb8b2d739b78a84c684d16f4be8e3f8d
Jakob Petermandl <e11776823@student.tuwien.ac.at>
2023-01-07 18:47:19 +0100

    add support for other events

--- src/main/java/ac/at/tuwien/streamingsocketbackend/SocketIOController.java
+++ src/main/java/ac/at/tuwien/streamingsocketbackend/SocketIOController.java
@@ -24,6 +24,8 @@ public class SocketIOController {
         this.server.addConnectListener(onUserConnectWithSocket);
         this.server.addDisconnectListener(onUserDisconnectWithSocket);
         this.server.addEventListener("getAllDashboardData", String.class, getAllDashboardData);
+        this.server.addEventListener("getSpeedingTaxis", String.class, getSpeedingTaxis);
+        this.server.addEventListener("getAreaViolations", String.class, getAreaViolations);
     }
 
     Logger log = Logger.getLogger(SocketIOController.class.getName());
@@ -46,21 +48,47 @@ public class SocketIOController {
         @Override
         public void onData(SocketIOClient socketIOClient, String message, AckRequest ackRequest) throws Exception {
             log.info("Client " + socketIOClient.getSessionId() + " requested data");
-            socketIOClient.sendEvent("getAllDashboardData", generateRandomValues().toString());
+            socketIOClient.sendEvent("getAllDashboardData", generateRandomDashboardValues().toString());
         }
     };
 
-    private List<DashboardEntity> generateRandomValues(){
+    public DataListener<String> getSpeedingTaxis = new DataListener<String>() {
+        @Override
+        public void onData(SocketIOClient socketIOClient, String message, AckRequest ackRequest) throws Exception {
+            log.info("Client " + socketIOClient.getSessionId() + " requested data");
+            socketIOClient.sendEvent("getSpeedingTaxis", generateRandomSpeedingAndAreaViolationValues(5).toString());
+        }
+    };
+
+    public DataListener<String> getAreaViolations = new DataListener<String>() {
+        @Override
+        public void onData(SocketIOClient socketIOClient, String message, AckRequest ackRequest) throws Exception {
+            log.info("Client " + socketIOClient.getSessionId() + " requested data");
+            socketIOClient.sendEvent("getAreaViolations", generateRandomSpeedingAndAreaViolationValues(3).toString());
+        }
+    };
+
+    private List<DashboardEntity> generateRandomDashboardValues(){
         List<DashboardEntity> dashboardEntities = new ArrayList<>();
         int id = 1;
         double lat = 39.908829698;
         double lng = 116.387665116;
         double distance = 1;
         double avgSpeed = 10;
-        for (double i = 0; i < 1; i += 0.1) {
+        for (double i = 0; i < 1; i += 0.05) {
             dashboardEntities.add(new DashboardEntity(String.valueOf(id), "Taxi" + id, lat+i, lng-i, distance+id, avgSpeed+id));
             id++;
         }
         return dashboardEntities;
+    }
+
+    private List<String> generateRandomSpeedingAndAreaViolationValues(int amount){
+        List<String> speedingAndAreaViolationValues = new ArrayList<>();
+        double id;
+        for (double i = 0; i < amount; i++) {
+            id = Math.random()*1000;
+            speedingAndAreaViolationValues.add("{\"id\":\"" + "Taxi"+ Math.round(id) + "\"}");
+        }
+        return speedingAndAreaViolationValues;
     }
 }
